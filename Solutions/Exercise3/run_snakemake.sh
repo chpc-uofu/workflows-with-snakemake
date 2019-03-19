bdf18e0bdefaf3a2e8ec8f6b275a42e2a087f6ae
Brett Milash <brett.milash@utah.edu>
2019-03-19 12:36:27 -0600

    Updated Exercise3. Added slides.

--- Solutions/Exercise3/run_snakemake.sh
+++ Solutions/Exercise3/run_snakemake.sh
@@ -6,5 +6,5 @@ module load snakemake/5.4.2
 
 snakemake -s exercise2-3.snakefile \
 	--cluster-config cluster.yaml \
-	--cluster "sbatch -p {cluster.partition} -A {cluster.account} -N {cluster.nodes} -J {rule} -t {cluster.time}" \
-	--jobs 4
+	--cluster "sbatch --res=snakemake2019 -M {cluster.name} -p {cluster.partition} -A {cluster.account} -N {cluster.nodes} -J {rule} -t {cluster.time}" \
+	--jobs 3
